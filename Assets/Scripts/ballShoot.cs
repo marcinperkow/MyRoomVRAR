8ae56e88dbe613855a563e91ec48be64ae1a7e8f
mperko6 <55112448+mperko6@users.noreply.github.com>
2020-10-17 17:06:04 -0500

    Final push

---
+++ Assets/Scripts/ballShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballShoot : MonoBehaviour
+{
+    public GameObject cannon;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBall = GameObject.Instantiate(ball, cannon.transform.position, cannon.transform.rotation) as GameObject;
+            newBall.GetComponent<Rigidbody>().velocity += Vector3.up * 2;
+            newBall.GetComponent<Rigidbody>().AddForce(newBall.transform.forward * 300);
+        }
+    }
+}
---
+++ Assets/Scripts/buttonAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class buttonAnimation : MonoBehaviour
+{
+    public GameObject vbBtnObj;
+    public AudioSource voiceObject;
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        vbBtnObj = GameObject.Find("Button");
+        voiceObject = GetComponent<AudioSource>(); vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed); vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        Debug.Log("start");
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed");
+        
+        if (anim.GetBool("isPressed"))
+        {
+            anim.SetBool("isPressed", false);
+        }
+        else
+        {
+            anim.SetBool("isPressed", true);
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");
+        voiceObject.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
